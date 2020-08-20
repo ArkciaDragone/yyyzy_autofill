5d71c1630afd96ead03e1c2d9896f2f9a2128a41
Arkcia <Arkcia@126.com>
2020-08-20 14:16:39 +0800

    not on campus & repeat yesterday

---
+++ README.md
@@ -0,0 +1,11 @@
+# YYYZY_Autofill
+
+This software basically repeats yesterday's submission. Currently only designed for "not on campus" situation. Code understanding is highly recommended.
+
+How to use:
+ - `pip install -r requirements.txt`
+ - Run the file
+
+Student ID and password are stored locally in `settings.json`. Please keep them safe.
+
+*By using this software, you understand and agree that the author does not assume any responsibility for possible consequences. See the license for complete terms.*
---
+++ yyyzy_autofill.py
@@ -0,0 +1,104 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+from random import random
+from datetime import date
+from pprint import pprint
+
+SETTINGS = None
+URLs = None
+
+
+def load_settings(filename='settings.json'):
+    global SETTINGS, URLs
+    with open(filename, encoding='utf-8') as f:
+        SETTINGS = json.load(f)
+        URLs = SETTINGS['urls']
+    changed = False
+    if not SETTINGS['login_data']['userName']:
+        SETTINGS['login_data']['userName'] = input('学号: ')
+        changed = True
+    if not SETTINGS['login_data']['password']:
+        SETTINGS['login_data']['password'] = input('密码: ')
+        changed = True
+    if changed:
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(SETTINGS, f, indent=4)
+
+
+def get_session():
+    session = requests.Session()
+    session.headers = SETTINGS['headers']
+    return session
+
+
+def __save_as_html(resp):
+    with open(f"debug.html", 'wb') as f:
+        f.write(resp.content)
+
+
+def login(s):
+    s.head(URLs['login_portal'])
+    s.head(URLs['login_oauth'])
+    json = s.post(URLs['login_post'], SETTINGS['login_data']).json()
+    assert json['success'], 'login failed'
+    s.head(f'{URLs["login_do"]}?_rand={random()}&token={json["token"]}')
+
+
+def update_check(session):
+    """Check whether data format has changed"""
+    resp = session.get(URLs['app_entry'])
+    soup = BeautifulSoup(resp.content, 'lxml')
+    for script in soup.find_all('script'):
+        if 'Yqfk.js' in script.get('src', ''):
+            s = script
+            break
+    else:
+        assert False, 'Yqfk.js not found'
+    yqfk = session.get(URLs['ssop'] + s['src'])
+    if yqfk.headers['Last-Modified'] != SETTINGS['yqfk_version']:
+        print('Warning: Yqfk.js has been modified')
+    js = str(yqfk.content, encoding='utf-8')
+    js = js[js.index('saveMrtb:'):]
+    js = js[js.index('reqData'):]
+    req = js[js.index('{')+3:js.index('}')].replace(' ', '').replace('\t', '')
+    keys = set(filter(lambda x: '//' not in x,
+                      (l.split(':')[0] for l in req.splitlines())))
+    assert keys == set(SETTINGS['upload_data'].keys()),\
+        'ATTENTION REQUIRED: reqData format changed'
+
+
+def get_today_upload_data(session):
+    data = SETTINGS['upload_data']
+    json = session.get(URLs['app_info']).json()
+    for k in data.keys():
+        if json['zrtbxx'].get(k):
+            data[k] = json['zrtbxx'][k]
+    data['tbrq'] = date.today().strftime('%Y%m%d')  # important
+    return data
+
+
+def upload(session):
+    data = get_today_upload_data(session)
+    pprint(data)
+    if 'required' in data.values():
+        print('Some of the "required" items are not filled. ' +
+              'Please fill and save the form manually for once and/or modify settings.')
+    else:
+        print('The following data will be uploaded:')
+        ins = input('Confirm? [y]|n:').strip().lower()[0]
+        if ins == '' or ins == 'y':
+            json = session.post(URLs['app_post']).json()
+            assert json['success'], 'upload failure: ' + \
+                json.get('msg', 'no msg')
+        else:
+            print('Aborted.')
+
+
+if __name__ == "__main__":
+    load_settings()
+    session = get_session()
+    print('Loading...')
+    login(session)
+    update_check(session)
+    upload(session)
